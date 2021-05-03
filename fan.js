b21384abc141a9bc01a374eb0c2b2bf5545d8f28
Navya140311 <72936140+Navya140311@users.noreply.github.com>
2021-05-03 22:23:47 +0530

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,25 @@
+status = "";
+
+function preload() {
+    img = loadImage('fan.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
